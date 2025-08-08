cc03ed2192dd83e5712656cb1954b3319c68f6a5
Bishoy Adel <bishoy.adel5555@gmail.com>
2025-08-08 19:31:44 +0200

    feat: added counter model

--- src/models/counter.js
+++ src/models/counter.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 const counterSchema = new mongoose.Schema({
-  _id: { type: String, required: true }, // e.g., 'clientId', 'projectId'
+  _id: { type: String, required: true },
   seq: { type: Number, default: 0 },
 });
 
-// Export the model, or existing one if already compiled
 module.exports =
   mongoose.models.Counter || mongoose.model("Counter", counterSchema);
